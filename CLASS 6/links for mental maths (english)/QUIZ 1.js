6f69c9a24bf86f052b676fd5cecb0f6fc3a8faf9
mathplanet1729 <84964861+mathplanet1729@users.noreply.github.com>
2021-06-13 16:53:32 +0530

    Add files via upload

---
+++ CLASS 6/links for mental maths (english)/QUIZ 1.js
@@ -0,0 +1,127 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "y + 30 = 70",
+    answer: "40",
+    options: [
+      "40",
+      "100",
+      "30",
+      "70"
+    ]
+  },
+    {
+    numb: 2,
+    question: "50 - (4 x 10)",
+    answer: "10",
+    options: [
+      "20",
+      "50",
+      "40",
+      "10"
+    ]
+  },
+    {
+    numb: 3,
+    question: "70 x 0.5",
+    answer: "35",
+    options: [
+      "3",
+      "35",
+      "350",
+      "45"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the value of the 4 in the number 549 632",
+    answer: "40,000",
+    options: [
+      "hundred thousands",
+      "ten thousands",
+      "40,000",
+      "400,000"
+    ]
+  },
+    {
+    numb: 5,
+    question: " y - 60 = 10",
+    answer: "70",
+    options: [
+      "40",
+      "50",
+      "70",
+      "80"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Even numbers are multiples of 2 E.g. 0,2,4,6..",
+    answer: "True",
+    options: [
+      "True",
+      "False",
+      "Can't Say",
+      "Not Defined"
+    ]
+  },
+  {
+    numb: 7,
+    question: "A class of students count the number of sea stars they find on a field trip to the beach. They then make a pictograph that shows how many sea stars each student found. The key to the pictograph is shown that is 1 star picture= 5 sea stars. If the pictograph has 20 sea stars, how many total sea stars were found by the students? ⭐=5",
+    answer: "100",
+    options: [
+      "50",
+      "75",
+      "100",
+      "150"
+    ]
+  },
+  {
+    numb: 8,
+    question: "¹²∕₂₀ Simplify this fraction",
+    answer: "³∕₅",
+    options: [
+      "²∕₁₀",
+      "⁴∕₅",
+      "³∕₅",
+      "⁶∕₁₁"
+    ]
+  },
+  {
+    numb: 9,
+    question: "The space programme has 4 astronauts in outer space and 14 astronauts on Earth. What is the ratio of the number of astronauts in outer space to the number of astronauts on Earth?",
+    answer: "4:14",
+    options: [
+      "14:4",
+      "28:4",
+      "4:14",
+      "18:8"
+    ]
+  },
+  {
+    numb: 10,
+    question: "?",
+    answer: "?",
+    options: [
+      "?",
+      "?",
+      "?",
+      "?"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
